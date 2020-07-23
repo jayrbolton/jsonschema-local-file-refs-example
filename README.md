05e59b8785b51af449fe73f263c5c758e9cff24b
Jay R Bolton <jayrbolton@gmail.com>
2020-07-23 10:02:51 -0700

    update

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
-# JSON-Schema Python local relative file references example
+# JSON-Schema Python RefResolver Example
+
+Resolve an arbitrary uri to a local file path, as well as any other sibling or child paths.
 
 See `main.py`
--- main.py
+++ main.py
@@ -4,13 +4,15 @@ import os
 import pathlib
 
 dirpath = pathlib.Path().absolute()
-base_schema_filename = "schema.json"
-base_schema_path = os.path.join(dirpath, base_schema_filename)
+base_schema_filename = "defs1.json"
+base_schema_path = os.path.join(dirpath, "base", base_schema_filename)
 
 with open(base_schema_path) as fd:
     base_schema = json.load(fd)
 
-resolver = jsonschema.RefResolver(f"file://{dirpath}/", base_schema)
+base_uri = f"http://example.com/schemas/defs1.json"
+
+resolver = jsonschema.RefResolver(base_uri, base_schema)
 validator = jsonschema.Draft7Validator(base_schema, resolver=resolver)
 
 schema_filename = "schema.json"
@@ -20,7 +22,7 @@ with open(schema_path) as fd:
     schema = json.load(fd)
 
 example = {
-    "product_id": 666,
+    "product_id": "666",
     "amount": {"code": "xyz", "amount": 1.23}
 }
 
